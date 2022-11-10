c8303b68e7e7c77cae3ab4e9ef84825ccd2a942d
Pierre Bourdet <pierre.bourdet1@gmail.com>
2022-11-10 19:12:04 +0100

    Display tweets

--- src/apps/authentication/models.py
+++ src/apps/authentication/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=False, blank=False)
+    first_name = models.CharField(max_length=150, null=False, blank=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'first_name']
 
     class Meta:
         db_table = "auth_user"
--- src/apps/twitter_clone/views.py
+++ src/apps/twitter_clone/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from .models import Tweet
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html')
+    tweets = Tweet.objects.all()
+    context = {'tweets': tweets}
+    return render(request, 'pages/home.html', context=context)
