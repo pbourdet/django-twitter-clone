2297f0d6b129ae9fb0025c2f621369b5f552d90e
Pierre Bourdet <pierre.bourdet1@gmail.com>
2022-11-21 20:39:52 +0100

    Add login feature

--- src/apps/authentication/views.py
+++ src/apps/authentication/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def log_in(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    email = request.POST.get('email').lower()
+    user = authenticate(request, email=email, password=request.POST.get(key='password'))
+
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.error(request, 'Wrong credentials')
+        return render(request, 'login.html')
+
+def log_out(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect('home')
--- src/project/urls.py
+++ src/project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("apps.twitter_clone.urls")),
+    path("", include("apps.authentication.urls")),
     path('admin/', admin.site.urls),
 ]
